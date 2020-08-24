a073f416ef9bf4ca879c84c9aa4e7a9dc571e5fb
chao.li <chao.li@team-lab.cn>
2020-08-24 11:08:30 +0800

    Initial Commit

---
+++ Hello_Metal01/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Hello_Metal01
+//
+//  Created by tlab on 2020/8/24.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
